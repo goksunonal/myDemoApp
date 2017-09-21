a753af434c8532f57e7a7e8d3d71d0dd81195f52
goksunonal <goksunonal@gmail.com>
2017-09-21 23:12:27 +0300

    Updated Demo

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,14 +12,21 @@ import spark.template.mustache.MustacheTemplateEngine;
 
 public class App
 {
-    public static boolean search(ArrayList<Integer> array, int e) {
-      System.out.println("inside search");
-      if (array == null) return false;
 
-      for (int elt : array) {
-        if (elt == e) return true;
-      }
-      return false;
+    public static boolean search(ArrayList<String> array,String se,int x){
+        if (array == null) return false;
+        int a=0;
+    	for (String elt : array) {
+    	        if (elt.equals(se)) {
+    	        	a++;
+    	        }
+    	}
+    	if(a==0 || a!=x){
+    		return false;
+    	}
+    	
+    		return true;
+    	
     }
 
     public static void main(String[] args) {
@@ -34,19 +41,20 @@ public class App
           String input1 = req.queryParams("input1");
           java.util.Scanner sc1 = new java.util.Scanner(input1);
           sc1.useDelimiter("[;\r\n]+");
-          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+          java.util.ArrayList<String> inputList = new java.util.ArrayList<>();
           while (sc1.hasNext())
           {
-            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+        	  String value = sc1.next().replaceAll("\\s","");
             inputList.add(value);
           }
           System.out.println(inputList);
 
 
           String input2 = req.queryParams("input2").replaceAll("\\s","");
-          int input2AsInt = Integer.parseInt(input2);
+          String input3=req.queryParams("input3").replaceAll("\\s","");
+          int input3AsInt = Integer.parseInt(input3);
 
-          boolean result = App.search(inputList, input2AsInt);
+          boolean result = App.search(inputList, input2,input3AsInt);
 
          Map map = new HashMap();
           map.put("result", result);
@@ -71,4 +79,5 @@ public class App
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
+
 
